387d01895110ea3ff721aa27fa9a32dc8852ba53
JUNG KAO <aronkao0115@gmail.com>
2024-07-24 01:46:16 +0800

    修改登入方式，以修正登入緩慢問題

---
+++ Aron.GrassMiner/Models/GrassLoinResp.cs
@@ -0,0 +1,23 @@
+﻿namespace Aron.GrassMiner.Models
+{
+
+    public class GrassLoinResp
+    {
+        public Result result { get; set; }
+    }
+
+    public class Result
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string accessToken { get; set; }
+        public string refreshToken { get; set; }
+        public string userId { get; set; }
+        public string email { get; set; }
+        public string userRole { get; set; }
+    }
+
+}
